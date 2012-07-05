7581fdfec62fd0210ec5e26c3d3c2bb65c64615c
Handrus Stephan Nogueira <handrus@gmail.com>
2012-07-05 15:47:05 -0300

    dojo 05/07 caixa eletronico com notas e saldo finito

--- caixa_eletronico/js/randori_action/js/caixa_eletronico.js
+++ caixa_eletronico/js/randori_action/js/caixa_eletronico.js
@@ -1,12 +1,23 @@
 // Your lib code.
 var caixa =
 {
+	notas: [100, 50, 10, 20],
+	notas_infinitas: true,
+	setQtdNotas: function(notas)
+	{
+		caixa.notas = [];
+		caixa.notas_infinitas = false;
+		for (var nota in notas) {
+			var qtd_notas = notas[nota];
+			for (var i = 0; i < qtd_notas; i++) {
+				caixa.notas.push(nota);
+			}
+		}
+		//console.log(caixa.notas);
+	},
 	saque: function(ammount)
 	{
-		var notas = ['100', '50', '20', '10'];
-
-		if(ammount % 10 != 0 || ammount <= 0)
-		{
+		if(ammount % 10 != 0 || ammount <= 0) {
 		    throw {
 		        name: "Error",
       		    message: "valor invalido"
@@ -15,16 +26,34 @@ var caixa =
 
 		var retorno = {};
 
-		for (nota in notas) {
-			var numeric = parseInt(nota);
-			if (ammount > nota) {
-				retorno[nota] = parseInt(ammount / numeric);
-				ammount -= numeric * retorno[nota];
+		if(caixa.notas.indexOf(ammount) != -1)
+		{
+			retorno[ammount] = 1;
+			return retorno;
+		}
+
+		var notas = caixa.notas.sort(function(a, b){ return b-a; });
+
+		for (var nota in notas) {
+			nota = notas[nota];
+
+			if ((ammount >= nota) && (caixa.notas_infinitas)) {
+				retorno[nota] = parseInt(ammount / nota);
+				ammount -= nota * retorno[nota];
+			}
+			else {
+				if (ammount >= nota) {
+					retorno[nota] = retorno[nota] +1;
+					ammount -= nota;
+				}
+			}
+
+			if(ammount == 0) {
+				break;
 			}
 		}
 
 		return retorno;
-
 	}
 }
 
--- caixa_eletronico/js/randori_action/tests/spec/caixa_eletronico_spec.js
+++ caixa_eletronico/js/randori_action/tests/spec/caixa_eletronico_spec.js
@@ -4,15 +4,25 @@ describe("Caixa Eletronico", function() {
   });
 
   //You must remove the x from the beginning to run the test.
-    it("Valores devem ser multiplos de 10).", function() {
-   		expect(function(){caixa.saque(21); }).toThrow(new Error('valor invalido'));
-   		expect(function(){caixa.saque(100.75)}).toThrow(new Error('valor invalido'));
-  	});
-    it("Valores devem ser positivos.", function(){
-    	expect(function(){caixa.saque(0);}).toThrow(new Error('valor invalido'));
-    	expect(function(){caixa.saque(-10);}).toThrow(new Error('valor invalido'));
-    });
-	it("Deve sacar o menor número de notas", function(){
-    	expect(caixa.saque(80)).toEqual({'50': 1, '20': 1, '10': 1});
-    });
+  it("Valores devem ser multiplos de 10).", function() {
+  		expect(function(){caixa.saque(21); }).toThrow(new Error('valor invalido'));
+  		expect(function(){caixa.saque(100.75)}).toThrow(new Error('valor invalido'));
+  });
+
+  it("Valores devem ser positivos.", function(){
+  	expect(function(){caixa.saque(0);}).toThrow(new Error('valor invalido'));
+  	expect(function(){caixa.saque(-10);}).toThrow(new Error('valor invalido'));
+  });
+
+  it("Deve sacar o menor número de notas", function(){
+    expect(caixa.saque(80)).toEqual({'50': 1, '20': 1, '10': 1});
+    expect(caixa.saque(70)).toEqual({'50': 1, '20': 1});
+    expect(caixa.saque(50)).toEqual({'50': 1});
+  });
+
+  it("Deve sacar o menor número de notas com notas finitas", function(){
+
+    caixa.setQtdNotas({'100' : 1, '50' : 1, '20' : 2, '10' : 5});
+    expect(caixa.saque(200)).toEqual({'100': 1, '50': 1, '20': 2, '10': 1});
+  });
 });
