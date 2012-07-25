fde57c3adf0b96600e38b61b9b773c2bf322bb82
iluz <iluz@ciandt.com>
2012-07-24 21:14:56 -0300

    Fixing intervals_test.py contents. Had overwritten its contents by mistake

--- intervalos/python/intervals_test.py
+++ intervalos/python/intervals_test.py
@@ -1,48 +1,85 @@
-class IntervalsFormatter():
-    @staticmethod
-    def format(interval):
-        sorted_interval = list(interval)
-        sorted_interval.sort()
+import unittest
+from intervals import IntervalsExtractor, IntervalsFormatter
 
-        if (len(sorted_interval) == 0):
-            formatted_interval = '[]'
-        elif (len(sorted_interval) <= 1):
-            formatted_interval = "[%s]" % sorted_interval[0]
-        else:
-            formatted_interval = "[%s-%s]" % (sorted_interval[0], sorted_interval[-1])
+class TestIntervalsFormatter(unittest.TestCase):
+    def test_format_empty_interval(self):
+        numbers = []
+        formatted_interval = IntervalsFormatter.format(numbers)
 
-        return formatted_interval
+        self.assertEqual('[]', formatted_interval)
+
+    def test_format_interval_with_single_item(self):
+        numbers = [1]
+        formatted_interval = IntervalsFormatter.format(numbers)
+
+        self.assertEqual('[1]', formatted_interval)
+
+    def test_format_interval_with_two_items(self):
+        numbers = [1, 2]
+        formatted_interval = IntervalsFormatter.format(numbers)
+
+        self.assertEqual('[1-2]', formatted_interval)
+
+    def test_format_interval_with_many_items(self):
+        numbers = [1, 2, 3, 4, 5, 6]
+        formatted_interval = IntervalsFormatter.format(numbers)
+
+        self.assertEqual('[1-6]', formatted_interval)
+
+    def test_format_unsorted_interval(self):
+        numbers = [2, 1, 4, 3, 6, 5]
+        formatted_interval = IntervalsFormatter.format(numbers)
+
+        self.assertEqual('[1-6]', formatted_interval)
+
+    def test_given_interval_is_not_changed(self):
+        original_numbers = [2, 1, 4, 3, 6, 5]
+        numbers = [2, 1, 4, 3, 6, 5]
+        IntervalsFormatter.format(numbers)
+        self.assertEqual(original_numbers, numbers)
 
 
-class IntervalsExtractor():
-    @staticmethod
-    def extract(numbers):
-        formatted_intervals_list = IntervalsExtractor.get_formatted_intervals_list(numbers)
+class TestIntervalsExtractor(unittest.TestCase):
+    def test_single_interval_unsorted(self):
+        numbers = [2, 1, 4, 3, 6, 5]
+        intervals = IntervalsExtractor.extract(numbers)
+        self.assertEqual('[1-6]', intervals)
 
-        return ', '.join(formatted_intervals_list)
+    def test_two_intervals(self):
+        numbers = [1, 2, 3, 6, 7, 8]
+        intervals = IntervalsExtractor.extract(numbers)
+        self.assertEqual('[1-3], [6-8]', intervals)
 
-    @staticmethod
-    def extract_intervals_list(numbers):
-        result_list = []
+    def test_multiple_intervals(self):
+        numbers = [100, 101, 102, 103, 104, 105, 110, 111, 113, 114, 115, 150]
+        intervals = IntervalsExtractor.extract(numbers)
+        expected_output = '[100-105], [110-111], [113-115], [150]'
+        self.assertEqual(expected_output, intervals)
 
-        sorted_numbers = list(numbers)
-        sorted_numbers.sort()
-        for index, current_number in enumerate(sorted_numbers):
-            is_sequential = (current_number - sorted_numbers[index - 1] == 1)
+    def test_multiple_intervals_from_unsorted_list(self):
+        numbers = [150, 101, 100, 104, 103, 102, 110, 105, 114, 113, 111, 115]
+        intervals = IntervalsExtractor.extract(numbers)
+        expected_output = '[100-105], [110-111], [113-115], [150]'
+        self.assertEqual(expected_output, intervals)
 
-            if (not is_sequential):
-                result_list.append([])
+    def test_given_list_is_not_changed(self):
+        original_numbers = [2, 1, 4, 3, 6, 5]
+        numbers = [2, 1, 4, 3, 6, 5]
+        IntervalsExtractor.extract(numbers)
+        self.assertEqual(original_numbers, numbers)
 
-            result_list[-1].append(current_number)
+    def test_get_formatted_intervals_list(self):
+        numbers = [1, 2, 3, 4, 7, 8, 9, 12, 13, 17]
+        formatted_interval_list = IntervalsExtractor.get_formatted_intervals_list(numbers)
+        expected_intervals_list = ['[1-4]', '[7-9]', '[12-13]', '[17]']
+        self.assertEqual(formatted_interval_list, expected_intervals_list)
 
-        return result_list
+    def test_get_formatted_intervals_list_from_unsorted_list(self):
+        numbers = [3, 2, 1, 4, 8, 7, 9, 17, 12, 13]
+        formatted_interval_list = IntervalsExtractor.get_formatted_intervals_list(numbers)
+        expected_intervals_list = ['[1-4]', '[7-9]', '[12-13]', '[17]']
+        self.assertEqual(formatted_interval_list, expected_intervals_list)
 
-    @staticmethod
-    def get_formatted_intervals_list(numbers):
-        formatted_intervals = []
 
-        intervals_list = IntervalsExtractor.extract_intervals_list(numbers)
-        for interval in intervals_list:
-            formatted_intervals.append(IntervalsFormatter.format(interval))
-
-        return formatted_intervals
+if __name__ == '__main__':
+    unittest.main()
