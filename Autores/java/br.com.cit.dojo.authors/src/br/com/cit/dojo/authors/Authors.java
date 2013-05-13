93e22631b10e867e784ff42fb026e5602739d739
larruda <lunascar@gmail.com>
2013-05-12 22:15:12 -0300

    Changes from Dojo on April 2nd 2013.

--- Autores/java/br.com.cit.dojo.authors/src/br/com/cit/dojo/authors/Authors.java
+++ Autores/java/br.com.cit.dojo.authors/src/br/com/cit/dojo/authors/Authors.java
@@ -1,5 +1,7 @@
 package br.com.cit.dojo.authors;
 
+import java.lang.reflect.Array;
+
 /**
  * 
  * @author Dojo
@@ -8,37 +10,34 @@ package br.com.cit.dojo.authors;
  *         publicações é comum que se apresente o nome do autor ou dos autores
  *         da seguinte forma: sobrenome do autor em letras maiúsculas, seguido
  *         de uma vírgula e da primeira parte do nome apenas com as iniciais
- *         maiúsculas. 
- *         
- *         Por exemplo: 
- *         
- *         SILVA, Joao 
- *         COELHO, Paulo 
- *         ARAUJO, Celso de
- *         
+ *         maiúsculas.
+ * 
+ *         Por exemplo:
+ * 
+ *         SILVA, Joao COELHO, Paulo ARAUJO, Celso de
+ * 
  *         Seu desafio é fazer um programa que leia um número inteiro
  *         correspondendo ao número de nomes que será fornecido, e, a seguir,
  *         leia estes nomes (que podem estar em qualquer tipo de letra) e
- *         imprima a versão formatada no estilo exemplificado acima. 
- *         
+ *         imprima a versão formatada no estilo exemplificado acima.
+ * 
  *         As seguintes regras devem ser seguidas nesta formatação: o sobrenome
  *         será igual a última parte do nome e deve ser apresentado em letras
- *         maiúsculas; 
- *         
- *         se houver apenas uma parte no nome, ela deve ser
- *         apresentada em letras maiúsculas (sem vírgula): se a entrada for “
- *         Guimaraes” , a saída deve ser “ GUIMARAES”; 
- *         
- *         se a última parte do nome
- *         for igual a "FILHO", "FILHA", "NETO", "NETA", "SOBRINHO", "SOBRINHA"
- *         ou "JUNIOR" e houver duas ou mais partes antes, a penúltima parte
- *         fará parte do sobrenome. 
- *         
- *         Assim: se a entrada for "Joao Silva Neto", a
- *         saída deve ser "SILVA NETO, Joao" ; se a entrada for "Joao Neto" , a
- *         saída deve ser "NETO, Joao"; as partes do nome que não fazem parte do
- *         sobrenome devem ser impressas com a inicial maiúscula e com as demais
- *         letras minúsculas; "da", "de", "do", "das", "dos" não fazem parte do
+ *         maiúsculas;
+ * 
+ *         se houver apenas uma parte no nome, ela deve ser apresentada em
+ *         letras maiúsculas (sem vírgula): se a entrada for “ Guimaraes” , a
+ *         saída deve ser “ GUIMARAES”;
+ * 
+ *         se a última parte do nome for igual a "FILHO", "FILHA", "NETO",
+ *         "NETA", "SOBRINHO", "SOBRINHA" ou "JUNIOR" e houver duas ou mais
+ *         partes antes, a penúltima parte fará parte do sobrenome.
+ * 
+ *         Assim: se a entrada for "Joao Silva Neto", a saída deve ser
+ *         "SILVA NETO, Joao" ; se a entrada for "Joao Neto" , a saída deve ser
+ *         "NETO, Joao"; as partes do nome que não fazem parte do sobrenome
+ *         devem ser impressas com a inicial maiúscula e com as demais letras
+ *         minúsculas; "da", "de", "do", "das", "dos" não fazem parte do
  *         sobrenome e não iniciam por letra maiúscula.
  * 
  */
@@ -47,7 +46,7 @@ public class Authors {
 	/**
 	 * @param args
 	 */
-	
+
 	private String[] names;
 
 	/**
@@ -58,40 +57,74 @@ public class Authors {
 	}
 
 	/**
-	 * @param names the names to set
+	 * @param names
+	 *            the names to set
 	 */
 	public void setNames(String[] names) {
 		this.names = names;
 	}
-	
 
 	public Authors(String[] names) {
 		this.names = names;
-		
-		if(!isValid())
+
+		if (!isValid())
 			throw new IllegalArgumentException("parâmetro names");
 		else {
 			formatName();
 		}
-			
+
 	}
-	
-	private boolean isValid(){
-		
+
+	private boolean isValid() {
+
 		for (int i = 0; i < this.names.length; i++) {
-			if ("".equals(names[i]))
-			{
+			if ("".equals(names[i])) {
 				return false;
-			}				
-		}		
+			}
+		}
 		return true;
 	}
-	
-	private void formatName()
-	{
+
+	private String camelCase(String name) {
+		name = name.toLowerCase();
+		char[] arrayName = name.toCharArray();
+		arrayName[0] = name.toUpperCase().toCharArray()[0];
+
+		return String.valueOf(arrayName);
+
+	}
+
+	private String formatWithThe(String name) {
+		String exception[] = { "de", "da", "do", "das", "dos" };
+		String[] littleBrokenName = name.split(" ");
+
+		return "";
+	}
+
+	private void formatName() {
+		String[] arraySplit;
+
 		for (int i = 0; i < this.names.length; i++) {
-			this.names[i] = this.names[i].toUpperCase();
+			arraySplit = this.names[i].split(" ");
+
+			if (arraySplit.length == 1) {
+				this.names[i] = this.names[i].toUpperCase();
+			} else if (arraySplit.length == 2) {
+				this.names[i] = arraySplit[arraySplit.length - 1].toUpperCase()
+						.concat(", ").concat(camelCase(arraySplit[0]));
+			} else if (arraySplit.length == 3) {
+
+				String withoutLastName = new String();
+				for (int j = 0; j < arraySplit.length - 2; j++) {
+					if (arraySplit[j].toLowerCase().equals("de")) {
+						withoutLastName += arraySplit[j];
+					} else {
+						withoutLastName += " " + camelCase(arraySplit[j]);
+					}
+				}
+				this.names[i] = arraySplit[arraySplit.length-1].toUpperCase().concat(", ").concat(withoutLastName);
+			}
 		}
 	}
-	
+
 }
