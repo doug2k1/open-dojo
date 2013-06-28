577e4d90665c0a6695e4f279d9542e98f436229d
larruda <lunascar@gmail.com>
2013-06-28 09:12:13 -0300

    More testing on scala for fraction problem on dojo meeting 27/06/13.

--- Fraction/scala/src/Fraction.scala
+++ Fraction/scala/src/Fraction.scala
@@ -1,22 +1,26 @@
 class Fraction(val x1: Int, val x2: Int)
 {
     def + (otherF:Fraction) = {
-      new Fraction(x1+otherF.x1, x2)
+      if (x2 == otherF.x2) {
+        new Fraction(x1 + otherF.x1, x2)
+      } else {
+    	  new Fraction(x1*otherF.x2 + x2*otherF.x1, x2*otherF.x2)
+      }
     }
     
     def simplified() : Fraction = {
       if(x1 == x2) {
         return new Fraction(1,1)
       }
-      this
+     
       
       if(x1 % x2 == 0) {
-       
-        
-      }  
-        val result =
-        return new   
-      }
+    	  return new Fraction(1, x2/x1)
+      } 
+      
+      this
+          
+      
     }
     override def toString() = {
       x1 + "/" + x2
--- Fraction/scala/test/FractionTest.scala
+++ Fraction/scala/test/FractionTest.scala
@@ -13,8 +13,40 @@ class Testing extends FlatSpec with ShouldMatchers{
     assert((x1 + x1).simplified().toString() === new Fraction(1,1).simplified().toString())
   }
   
-  "Sum of two halves simplified 2/4 + 2/4" should "be 1/1" in {
+  "Sum of two halves 2/4 + 2/4" should "be 4/4" in {
     val x1:Fraction = new Fraction(2,4)
-    assert((x1.simplified() +x1.simplified()).toString() === new Fraction(1,1).simplified().toString())
+    assert((x1 + x1).toString() === "4/4")
   }
+  
+  "Sum 1/2 (-1)/2" should "be 0/2" in {
+    val x1: Fraction = new Fraction(1,2)
+    val x2: Fraction = new Fraction(-1,2)
+
+    assert((x1 + x2).toString() === "0/2")
+  }
+
+  "Sum 1/2 1/3" should "be 5/6" in {
+    val x1: Fraction = new Fraction(1,2)
+    val x2: Fraction = new Fraction(1,3)
+    
+    assert((x1+x2).toString() === "5/6")
+  }
+  
+  "Sum of two negatives" should "also be negative" in {
+    assert((new Fraction(-1, 2) + new Fraction(-1, 2)).toString === "-2/2")
+  }
+  
+  it should "be negative even in denominator" in {
+    assert((new Fraction(1, -2) + new Fraction(1,-2)).toString === "2/-2")
+  }
+  
+  it should "be negative even if denominator differs" in {
+    assert((new Fraction(-1,2) + new Fraction(-1,3)).toString === "-5/6")
+  }
+  
+  it should "be negative even if both differs" in {
+    assert((new Fraction(-1,2) + new Fraction(-2,-1)).toString === "-3/-2")
+  }
+  
+  "Sum of 1/2 with 1/2" should "
 }
