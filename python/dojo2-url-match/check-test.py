184f0c7a9beb9f6443bacc76846afb5597a54f7d
Celso Andrade <eu@celsoandrade.net>
2014-04-03 09:34:26 -0300

    segundo dojo python

--- python/dojo2-url-match/check-test.py
+++ python/dojo2-url-match/check-test.py
@@ -2,19 +2,20 @@ import unittest
 from checkurl import checkurl
 
 class bissexto_test(unittest.TestCase):
-    def test_url_valida(self):
-        self.assertEqual(checkurl('http://10minutemail.com'),True)
-        self.assertEqual(checkurl('http://google.com'),True)
-        self.assertEqual(checkurl('https://fb.com'), True)
+  def test_url_valida(self):
+    self.assertEqual(checkurl('http://10minutemail.com'),True)
+    self.assertEqual(checkurl('http://google.com'),True)
+    self.assertEqual(checkurl('https://fb.com'), True)
+    self.assertEqual(checkurl('https://'))
 
-    def test_url_invalida(self):
-        self.assertEqual(checkurl('abc://www.test.com'),False)
-        self.assertEqual(checkurl('http:www.teste.com'),False)
-        self.assertEqual(checkurl('httpwww.teste.com'),False)
-        self.assertEqual(checkurl('http:/www.teste.com'),False)
-        self.assertEqual(checkurl('http://.www.teste.com'),False)
-        self.assertEqual(checkurl('http://'), False)
+  def test_url_invalida(self):
+    self.assertEqual(checkurl('abc://www.test.com'),False)
+    self.assertEqual(checkurl('http:www.teste.com'),False)
+    self.assertEqual(checkurl('httpwww.teste.com'),False)
+    self.assertEqual(checkurl('http:/www.teste.com'),False)
+    self.assertEqual(checkurl('http://.www.teste.com'),False)
+    self.assertEqual(checkurl('http://'), False)
 
 
 if __name__ == "__main__":
-    unittest.main()
+  unittest.main()
