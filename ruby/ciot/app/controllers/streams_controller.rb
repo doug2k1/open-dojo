68f8a9ecd2ca55b876310e9de717923dab027d3d
Douglas Matoso <doug2k1@gmail.com>
2014-08-05 12:54:27 +0000

    Dojo Rails (CIoT): Controller de novo Stream

--- ruby/ciot/app/controllers/streams_controller.rb
+++ ruby/ciot/app/controllers/streams_controller.rb
@@ -3,7 +3,14 @@ class StreamsController < ApplicationController
   
   def new
     key = request.headers['key']
-    device = Device.find_by_key(key)
+    device = Device.find_by_key(key) if key
     
+    unless device
+      render status: :bad_request and return
+    end
+    
+    device.streams.create(body: request.body.read)
+    
+    render status: :ok
   end
 end
